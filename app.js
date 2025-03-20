d552fdf84c3b0e5a1bbd729406f85b78ad308601
Ojas <ojas1807@gmail.com>
2025-03-19 23:54:52 -0400

    I really should be studying for my midterm right now

---
+++ app.js
@@ -0,0 +1,9 @@
+// TO DO 
+//
+// Make a thing that does all the things that you want to do 
+//
+//
+// AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+//
+//
+// I should really be studying for my midterm
